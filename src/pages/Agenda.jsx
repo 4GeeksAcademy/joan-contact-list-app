0a409c5d81000306049f215a4a25c8c7a8871bdd
Joan <79165234+Joangb85@users.noreply.github.com>
2024-12-21 15:16:56 +0000

    POST & DELETE calls implementation

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,48 +1,89 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { isEmpty } from "lodash";
+import { Container, Badge, Button } from "react-bootstrap";
 
 export const Agenda = () => {
-
     const [agendas, setAgendas] = useState([]);
+    const [inputValue, setInputValue] = useState(""); 
+
     const getContactList = () => {
-        fetch("https://playground.4geeks.com/contact/agendas/", {
+        fetch("https://playground.4geeks.com/contact/agendas", {
             method: "GET",
-        }).then((res) => {
-            return res.json()
-        }).then((response) => {
-            setAgendas(response.agendas)
         })
-    }
+            .then((res) => res.json())
+            .then((response) => {
+                setAgendas(response.agendas); 
+            });
+    };
+
+    const addNewContact = (slug) => {
+        fetch(`https://playground.4geeks.com/contact/agendas/${slug}`, {
+            method: "POST",
+            body: JSON.stringify({slug}), 
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then(() => getContactList());
+    };
+
+    const removeContacts = (slug) => {
+        fetch(`https://playground.4geeks.com/contact/agendas/${slug}`, {
+            method: "DELETE",
+        }).then(() => getContactList());
+    };
 
     useEffect(() => {
-        getContactList()
-    }, [])
+        getContactList(setAgendas);
+    }, []);
 
     return (
-        <>
-          {!isEmpty(agendas) && agendas.map((item)=> {
-            return (
-                <h1>{item.slug}</h1>
-            )
-          })}
-        </>       
-    )
-
-    const addNewContact =(text) => {
-        const contact = {
-            name: text,
-            phone: false
-            email
-        }
-        fetch('https://playground.4geeks.com/contact/agendas/Joan/contacts', {
-            method: "POST",
-            body: JSON.stringify(task),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(() => getTodosList())
-    }
-
-}
+        <Container className="mt-5">
+            <Badge
+                className="py-3 px-3 bg-success border rounded"
+                style={{
+                    width: "100%",
+                }}
+            >
+                <h1 className="mb-3 bg-success text-light d-flex align-items-center justify-content-start">
+                    To Do List
+                </h1>
+                <input
+                    type="text"
+                    placeholder="Write a new task"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.code === "Enter" && inputValue.trim() !== "") {
+                            addNewContact(inputValue);
+                            setInputValue("");
+                        }
+                    }}
+                    style={{
+                        padding: "10px",
+                        fontSize: "16px",
+                        borderRadius: "5px",
+                        width: "100%",
+                    }}
+                />
+            </Badge>
+            {!isEmpty(agendas) && agendas.map((element) => (
+                <Container
+                    className="d-flex align-items-center"
+                    key={element.slug} 
+                >
+                    <Container className="text-light mt-1 py-2 bg-success border rounded">
+                        {element.slug || "Sin etiqueta"}
+                        <Button
+                            className="text-success float-end"
+                            variant="light"
+                            size="sm"
+                            onClick={() => removeContacts(element.slug)}
+                        >
+                            <strong>X</strong>
+                        </Button>
+                    </Container>
+                </Container>
+            ))}
+        </Container>
+    );
+};
