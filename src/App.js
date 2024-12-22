f28877cd894f251ba4e2cbfffddedc80b77163dc
Joan <79165234+Joangb85@users.noreply.github.com>
2024-12-22 17:47:13 +0000

    routeConfig implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import "./App.css";
 import { Agenda } from "./pages/Agenda";
-import Users from "./pages/Users";
+import { Users } from "./pages/Users";
 import { Routes, Route } from "react-router";
 
 const App = () => {
+  const routeConfig = [
+    {
+      name: "Root",
+      path: "/",
+      page: <Homepage />,
+    },
+    {
+      name: "Root",
+      path: "/Agenda",
+      page: <Agenda />,
+    },
+    {
+      name: "Root",
+      path: "/Users",
+      page: <Users />,
+    },
+  ];
   return (
     <Routes>
-      <Route path="/" element={<Agenda />} />
-      <Route path="/Users" element={<Users />} />
+      {routeConfig.map((route) => {
+        return <Route path={route.path} element={route.page} />;
+      })}
     </Routes>
   );
 };
